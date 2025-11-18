99e2560401a76641e211551d24e1874c713eaee2
786-Usa <musaabbas900@gmail.com>
2025-11-18 22:36:01 +0500

    feat: implement user login functionality with validation and error handling

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -38,4 +38,34 @@ const registerUser = async (req, res) => {
     }
 
 }
-export { registerUser };
+
+const loginUser = async (req,res)=>{
+    try {
+        const {email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        const userValidation = await User.findOne({ email })
+        if(!userValidation){
+            return res.status(404).json({message:"User not found"});
+        }
+        const isPasswordValid = await bcrypt.compare(password,userValidation.password);
+        if(!isPasswordValid){
+            return res.status(401).json({message:"Invalid credentials"});
+        }
+        const userRes = {
+            _id: userValidation._id,
+            username: userValidation.username,
+            email: userValidation.email
+        }
+        return res.status(200).json({message:"Login successful", userData:userRes});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error", error });
+    }
+}
+
+
+export { registerUser,
+    loginUser
+ };
